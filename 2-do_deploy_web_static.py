8eb15bd128517e6357b2d9a4e2b3d6672a6a0419
Evanice4 <n.karabaran@alustudent.com>
2024-10-05 18:48:29 +0200

    Corrected the ip address

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['3.93.73.92', '23.20.102.82']
+env.hosts = ['3.88.138.206', '54.242.7.128']
 
 
 def do_deploy(archive_path):
