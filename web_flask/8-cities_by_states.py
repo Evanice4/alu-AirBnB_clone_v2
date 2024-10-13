b0c95728bca16d2be5b40484492a4553c5130407
Evanice4 <n.karabaran@alustudent.com>
2024-10-13 18:35:13 +0200

    PEP8 validation error correction

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+"""Flask"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+from models.city import City
+
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def states_list():
-    """Render template with states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-
-    # sort State object alphabetically by name
-    # sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, states=states)
-
-
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown_session(exception):
+    """ Teardown """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+@app.route('/cities_by_states', strict_slashes=False)
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html',
+                           Table="States",
+                           states=states)
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
