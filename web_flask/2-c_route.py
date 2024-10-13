6ed7277e0a9d65901fc60f86266a24691cbd0c3b
Evanice4 <n.karabaran@alustudent.com>
2024-10-13 18:00:35 +0200

    updated 2-c_route.py file

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""a script that starts a flask web application"""
+"""Flask"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """a function that prints hello `hbnb at the root"""
-    return "Hello HBNB!"
+    """prints Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Display HBNB at /hbnb"""
-    return "HBNB"
+    """Returns "HBNB" """
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space"""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+def c_text(text):
+    """Returns the string "C" followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
